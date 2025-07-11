ca3c88cc6b2032a4f47768cfed4693391feb8e04
Suraj-231 <suraj.menariya@strathmore.edu>
2025-07-11 23:56:18 +0300

    Delete challenges_task directory

--- challenges_task/Greeter.java
+++
@@ -1,9 +0,0 @@
-//puts message on screen
-public class Greeter{
-    void greet(){
-        System.out.println("Hi there");
-    }
-    public static void main(String[] args) {
-        (new Greeter()).greet();
-    }
-}
